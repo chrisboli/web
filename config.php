1e7e48d0434398764a07e2b06a9917e48ad2db48
chrisboly cerdas gómez <47968313+chrisboli@users.noreply.github.com>
2025-04-24 20:36:29 -0600

    Archivos
    Se subieron los archivos de la web

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$mysql = array(
+
+	"host" => "localhost",
+	"username" => "sarakion",
+	"password" => "Lidia30",
+	"auth" => "auth",
+
+);
+
+$site = array(
+
+	"title" => "Registration Page",
+	"meta_description" => "Jordythery's Blizzlike repack Account Registration",
+	"meta_keywords" => "Jordythery, Blizzlike, Repack, Registration",
+	"meta_robots" => "INDEX,NOFOLLOW",
+	"realmlist" => "set realmlist pruebasboli.fun"
+
+);
+
+?>
